140d47482bc49471ec0830657d82762cda333b55
freer821 <freergzy8212@hotmail.com>
2018-02-28 15:55:33 +0100

    change db name

--- mysql-docker.sh
+++ mysql-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Restore
-cat ./dump/ecshop.sql | docker exec -i 322ba4a114ab /usr/bin/mysql -u root --password=root123@#  ecshop
+cat ./dump/wp-kang.sql | docker exec -i 322ba4a114ab /usr/bin/mysql -u root --password=root123@#  wordpress
 # Backup
 #docker exec CONTAINER /usr/bin/mysqldump -u root --password=root DATABASE > backup.sql
