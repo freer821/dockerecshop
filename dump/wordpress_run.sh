313ee9102debd0fcaa9f4fa754041e330263e003
freer821 <freergzy8212@hotmail.com>
2018-03-27 19:44:19 +0200

    bugfixing

--- dump/wordpress_run.sh
+++ dump/wordpress_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "beginning to import wordpress database"
-mysql -u root-e "CREATE DATABASE wordpress /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+mysql -u root -e "CREATE DATABASE wordpress /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 mysql -u root wordpress < /app/mysql/wordpress_run.sql
 echo "import wordpress database end!"
